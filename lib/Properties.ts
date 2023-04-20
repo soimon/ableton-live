fec7e7346fc4dbd167cb10c6e903eea12d507ba5
soimon <soimon@soimon.com>
2023-04-20 16:12:07 +0200

    Fix

--- lib/Properties.ts
+++ lib/Properties.ts
@@ -1,166 +1,129 @@
-import { AbletonLive } from "./index";
+import { AbletonLive } from './index';
 
 interface ChildrenInitialProps {
-    name: string;
-    initialProps: string[];
+	name: string;
+	initialProps: string[];
 }
 
 export class Properties<GP, CP, TP, SP, OP> {
-    protected _state = new Map();
 
-    constructor(
-        protected ableton: AbletonLive,
-        protected ns: string,
-        protected path: string,
-        protected childrenInitialProps?: Partial<{
-            [T in keyof CP]: (string | ChildrenInitialProps)[];
-        }>,
-        protected _id?: number
-    ) {}
+	protected _state = new Map();
 
-    get id(): number | undefined {
-        return this._id;
-    }
+	constructor(
+		protected ableton: AbletonLive,
+		protected ns: string,
+		protected path: string,
+		protected childrenInitialProps?: Partial<{ [T in keyof CP]: (string | ChildrenInitialProps)[] }>,
+		protected _id?: number
+	) {}
 
-    getPath(): string {
-        return this.path;
-    }
+	get id(): number | undefined {
+		return this._id;
+	}
 
-    /**
-     * @private
-     * Under construction
-     */
-    // state<T extends keyof GP | keyof TP>(
-    // 	key: T
-    // ): T extends keyof TP ? undefined | TP[T] : T extends keyof GP ? undefined | GP[T] : any {
-    // 	return this._state.get(key);
-    // }
+	getPath():string {
+		return this.path;
+	}
 
-    protected getterTransformers: Partial<{
-        [T in keyof GP]: (val: GP[T]) => any;
-    }> = {};
-    protected childrenTransformers: Partial<{
-        [T in keyof CP]: (val: CP[T]) => any;
-    }> = {};
+	/**
+	 * @private
+	 * Under construction
+	 */
+	// state<T extends keyof GP | keyof TP>(
+	// 	key: T
+	// ): T extends keyof TP ? undefined | TP[T] : T extends keyof GP ? undefined | GP[T] : any {
+	// 	return this._state.get(key);
+	// }
 
-    async get<T extends keyof GP>(
-        prop: T
-    ): Promise<T extends keyof TP ? TP[T] : GP[T]> {
-        const result = await this.ableton.get(
-            this.path,
-            prop as string,
-            this._id
-        );
+	protected getterTransformers: Partial<{ [T in keyof GP]: (val: GP[T]) => any }> = {};
+	protected childrenTransformers: Partial<{ [T in keyof CP]: (val: CP[T]) => any }> = {};
 
-        const transformer = this.getterTransformers[prop];
+	async get<T extends keyof GP>(prop: T): Promise<T extends keyof TP ? TP[T] : GP[T]> {
+		const result = await this.ableton.get(this.path, prop as string, this._id);
 
-        let value = result;
+		const transformer = this.getterTransformers[prop];
 
-        if (result !== null && transformer) {
-            value = transformer(result);
-        }
+		let value = result;
 
-        // this._state.set(prop, value);
+		if (result !== null && transformer) {
+			value = transformer(result);
+		}
 
-        return value;
-    }
+		// this._state.set(prop, value);
 
-    async children<T extends keyof CP>(
-        child: T,
-        childProps?: string[],
-        index: number?
-    ): Promise<T extends keyof TP ? TP[T] : CP[T]> {
-        let initialProps;
+		return value;
+	}
 
-        if (this.childrenInitialProps) {
-            initialProps = this.childrenInitialProps[child];
-        }
+	async children<T extends keyof CP>(child: T, childProps?: string[], index?:number): Promise<T extends keyof TP ? TP[T] : CP[T]> {
+		let initialProps;
 
-        if (childProps) {
-            initialProps = initialProps.concat(childProps);
-        }
+		if (this.childrenInitialProps) {
+			initialProps = this.childrenInitialProps[child];
+		}
 
-        const result = await this.ableton.getChildren(
-            this.path,
-            { child: child as string, initialProps, index },
-            this._id
-        );
+		if (childProps) {
+			initialProps = initialProps.concat(childProps);
+		}
 
-        const transformer = this.childrenTransformers[child];
+		const result = await this.ableton.getChildren(this.path, { child: child as string, initialProps, index }, this._id);
 
-        if (result !== null && transformer) {
-            return transformer(result);
-        } else {
-            return result;
-        }
-    }
+		const transformer = this.childrenTransformers[child];
 
-    async child<T extends keyof CP>(
-        child: T,
-        index: number,
-        childProps?: string[]
-    ): Promise<(T extends keyof TP ? TP[T] : CP[T])[number] | undefined> {
-        const [result] = await this.children(child, index, childProps);
-        return result;
-    }
+		if (result !== null && transformer) {
+			return transformer(result);
+		} else {
+			return result;
+		}
+	}
 
-    async set<T extends keyof SP>(prop: T, value: SP[T]): Promise<null> {
-        return this.ableton.set(this.path, prop as string, value, this._id);
-    }
+	async child<T extends keyof CP>(child: T, index:number, childProps?: string[]): Promise<(T extends keyof TP ? TP[T] : CP[T])[number]|undefined> {
+		const [result] = await this.children(child, index, childProps);
+		return result;
+	}
 
-    async observe<T extends keyof OP | keyof CP>(
-        prop: T,
-        listener: (
-            data: T extends keyof OP ? OP[T] : T extends keyof TP ? TP[T] : any
-        ) => any
-    ): Promise<any> {
-        const child = prop as any;
-        const childTransformer = this.childrenTransformers[child as keyof CP];
-        const getterTransformer = this.getterTransformers[child as keyof GP];
+	async set<T extends keyof SP>(prop: T, value: SP[T]): Promise<null> {
+		return this.ableton.set(this.path, prop as string, value, this._id);
+	}
 
-        let initialProps;
+	async observe<T extends keyof OP | keyof CP>(
+		prop: T,
+		listener: (data: T extends keyof OP ? OP[T] : T extends keyof TP ? TP[T] : any) => any
+	): Promise<any> {
+		const child = prop as any;
+		const childTransformer = this.childrenTransformers[child as keyof CP];
+		const getterTransformer = this.getterTransformers[child as keyof GP];
 
-        const callback = (data) => {
-            if (data !== null && childTransformer) {
-                listener(childTransformer(data));
-            } else if (data !== null && getterTransformer) {
-                listener(getterTransformer(data));
-            } else {
-                listener(data);
-            }
-        };
+		let initialProps;
 
-        if (this.childrenInitialProps) {
-            initialProps = this.childrenInitialProps[child];
+		const callback = (data) => {
+			if (data !== null && childTransformer) {
+				listener(childTransformer(data));
+			} else if (data !== null && getterTransformer) {
+				listener(getterTransformer(data));
+			} else {
+				listener(data);
+			}
+		};
 
-            return this.ableton.observe(this.path, prop as string, callback, {
-                initialProps,
-                liveObjectId: this._id,
-            });
-        } else {
-            return this.ableton.observe(this.path, prop as string, callback, {
-                liveObjectId: this._id,
-            });
-        }
-    }
+		if (this.childrenInitialProps) {
+			initialProps = this.childrenInitialProps[child];
 
-    protected async call(
-        method: string,
-        parameters?: any[],
-        timeout?: number
-    ): Promise<any> {
-        return this.ableton.call(
-            this.path,
-            { parameters, method },
-            this._id,
-            timeout
-        );
-    }
+			return this.ableton.observe(this.path, prop as string, callback, {
+				initialProps,
+				liveObjectId: this._id,
+			});
+		} else {
+			return this.ableton.observe(this.path, prop as string, callback, {
+				liveObjectId: this._id,
+			});
+		}
+	}
 
-    protected async callMultiple(
-        calls: any[][],
-        timeout?: number
-    ): Promise<any> {
-        return this.ableton.callMultiple(this.path, calls, this._id, timeout);
-    }
+	protected async call(method: string, parameters?: any[], timeout?: number): Promise<any> {
+		return this.ableton.call(this.path, { parameters, method }, this._id, timeout);
+	}
+
+	protected async callMultiple(calls: any[][], timeout?: number): Promise<any> {
+		return this.ableton.callMultiple(this.path, calls, this._id, timeout);
+	}
 }
